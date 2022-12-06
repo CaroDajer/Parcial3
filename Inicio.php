3228dfa0e4e42de2a20f406ea4e8d56f6a14ba9d
Carolina Dajer <15209533@modelo.edu.mx>
2022-12-06 00:22:50 -0600

    1er commit

---
+++ Inicio.php
@@ -0,0 +1,16 @@
+//pagina que nos servirá de inicio
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    <p>probando</p>
+    <p>nombre del autor</p>
+    <p>foto</p>
+</body>
+<?php $conex=mysqli_connect("localhost", "root", "", "inicio") ?>
+</html>
---
+++ NuevArch.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuevo Archivot</title>
+</head>
+<body>
+    <?php echo esto es una prueba; ?>
+</body>
+<?php $conex=mysqli_connect("localhost", "root", "", "inicio") ?>
+</html>
